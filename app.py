4f9d49240f2b10de94b6174b4fcb613e1a3051ac
misaaaaal <misaalraikar1@gmail.com>
2024-10-14 17:35:08 +0530

    repo created

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+env_var=os.getenv('ENV_VAR')
+
+@app.route('/')
+def home():
+    return f'<h1>Environment variable: {env_var}</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
