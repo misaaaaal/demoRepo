1e1586a1ae6c42ee71229c138fc77598d209fa38
misaaaaal <misaalraikar1@gmail.com>
2024-10-15 15:58:01 +0530

    changes to content

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ env_var=os.getenv('ENV_VAR')
 
 @app.route('/')
 def home():
-    return f'<h1>Staging: {env_var}</h1>'
+    return f'<h1>Production: {env_var}</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
